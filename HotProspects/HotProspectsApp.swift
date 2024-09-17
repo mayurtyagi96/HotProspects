79ce1fb3f7db93ce366fce5a60be200ac3a0003e
Mayur <mayurtyagi@unolo.com>
2024-09-17 19:46:12 +0530

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Mayur  on 17/09/24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
