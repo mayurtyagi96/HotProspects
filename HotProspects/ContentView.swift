36e45125318f5d3b961997d0481534e860a3975a
mayurtyagi <mayurtyagi@unolo>
2024-09-18 20:53:43 +0530

    concept - Result and Task

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,42 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selectedTab = 2
+    @State private var output = "mayur"
     var body: some View {
-        NavigationStack{
-            TabView(selection: $selectedTab){
-                VStack{
-                    NavigationLink("next screen") {
-                        nextView()
-                    }
-                    Text("Tab 1")
-                    
-                }
-                .tabItem {
-                    Label("item 1", systemImage: "star")
-                }
-                .tag(1)
-                
-                VStack{
-                    Text("Tab 2")
-                    Button("to 1") {
-                        selectedTab = 1
-                    }
-                }
-                .tabItem {
-                    Label("item 2", systemImage: "star")
-                }
-                .tag(2)
+        Text(output)
+            .task {
+                await fetchReadings()
             }
-        }
+
     }
+    
+    func fetchReadings() async{
+        let fetchTask = Task {
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data,_) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            return "\(readings.count)"
+        }
+        
+        let result = await fetchTask.result
+        switch result{
+        case .success(let str):
+            output = str
+        case .failure(let err):
+            output = err.localizedDescription
+        }
+        
+    }
+    
+    
+    
+////    --------
+//    var s = Something {
+//        <#code#>
+//    }
+//    
+//    struct Something{
+//        var name: String = "mayur"
+//        var closure: () -> Void
+//    }
+////    ----------
 }
 
 #Preview {
     ContentView()
 }
-struct nextView: View {
-    var body: some View {
-        Text("hello folks")
-    }
-}
+
+
