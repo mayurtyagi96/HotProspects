77ab88a05096212ee7e1671e0c326592f7d6fe1f
Mayur <mayurtyagi@unolo.com>
2024-09-19 20:54:44 +0530

    3rd day completed

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -4,38 +4,18 @@
 //
 //  Created by Mayur  on 17/09/24.
 //
-import UserNotifications
+import SamplePackage
 import SwiftUI
 
 struct ContentView: View {
+    var elements = 1...60
     
+    var result: String {
+        let selected = elements.random(10).sorted()
+        return selected.map(String.init).formatted()
+    }
     var body: some View {
-        Button("Ask for permission"){
-            UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
-                if !success{
-                    print("permission not granted")
-                }
-                if success{
-                    print("All set")
-                }else if let error{
-                    print(error.localizedDescription)
-                }
-            }
-        }
-        .padding()
-        
-        Button("Create Notifiaction"){
-            let context = UNMutableNotificationContent()
-            context.title = "Feed the cat"
-            context.subtitle = "It looks hungry"
-            context.sound = .default
-            
-            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-            
-            let request = UNNotificationRequest(identifier: UUID().uuidString, content: context, trigger: trigger)
-            
-            UNUserNotificationCenter.current().add(request)
-        }
+        Text(result)
     }
 }
 
