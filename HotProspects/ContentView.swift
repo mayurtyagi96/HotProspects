c1fbf2648e81f49c3620e0e0aa2365a7bb99d902
mayurtyagi <mayurtyagi@unolo>
2024-09-23 21:15:56 +0530

    conflict resolved

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,12 +8,6 @@ import SamplePackage
 import SwiftUI
 
 struct ContentView: View {
-    var elements = 1...60
-    
-    var result: String {
-        let selected = elements.random(10).sorted()
-        return selected.map(String.init).formatted()
-    }
     var body: some View {
         Text("Project Started")
     }
